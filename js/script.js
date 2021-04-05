2a0470f0ee383cd7cba3db71163cc0484a3275aa
Daniel B <dbranquinhogomes@gmail.com>
2021-04-05 20:11:12 -0300

    Update var

--- js/script.js
+++ js/script.js
@@ -1,31 +1,27 @@
-function clicked(){
-var i;
-var id;
-var type;
-var level;
-var component;
+function clicked() {
+    let i, id, type, level, component;
 
-var prefix = (document.getElementsByClassName('module_info value')[0].textContent).split("/");
-level = prefix[1];
-component = prefix[0];
+    let prefix = (document.getElementsByClassName('module_info value')[0].textContent).split("/");
+    level = prefix[1];
+    component = prefix[0];
 
-if((document.getElementsByClassName('module_info title')[0].textContent).split(" -",1) == "REQ"){
-    doc = "REQ";
-}else{
-    doc = "ICD";
+    if ((document.getElementsByClassName('module_info title')[0].textContent).split(" -", 1) == "REQ") {
+        doc = "REQ";
+    } else {
+        doc = "ICD";
+    }
+
+    for (i = 0; i < 10; i++) {
+        type = document.getElementsByClassName('module body type')[i].textContent;
+        if (type == "Information") {
+            stop;
+        } else {
+
+            id = document.getElementsByClassName('module body id')[i].textContent;
+
+            document.getElementsByClassName('module body gmtid')[i].innerHTML = doc + "-" + level + "-" + component + "-" + id;
+        }
+    }
 }
 
-for (i=0;i<10;i++){
-    type = document.getElementsByClassName('module body type')[i].textContent;
-    if (type == "Information"){
-        stop;   
-    }else{
-
-        id = document.getElementsByClassName('module body id')[i].textContent;
-    
-        document.getElementsByClassName('module body gmtid')[i].innerHTML = doc+"-"+level+"-"+component+"-"+id;
-}
-}
-}
-
-document.getElementById("run").addEventListener("click",clicked);
+document.getElementById("run").addEventListener("click", clicked);
